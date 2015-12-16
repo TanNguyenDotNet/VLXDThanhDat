d8d1c3487e981fe53b6eebd625ba437d4675ca3f
anhphanhcm <anhphan.hcm@gmail.com>
2015-12-16 15:39:19 +0700

    add new

---
+++ MVCProject/Common/Commons.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProject.Common
+{
+    public class Commons
+    {
+        public static IEnumerable<SelectListItem> GetLocationList(Models.aspnetEntities db)
+        {
+            return db.Locations.AsEnumerable()
+                .Select(d => new SelectListItem
+                {
+                    Value = d.ID.ToString(),
+                    Text = d.LocationName
+                });
+        }
+
+        public static IEnumerable<SelectListItem> GetCatalogList(Models.aspnetEntities db, int? IgnoreID)
+        {
+            List<Models.Catalog> result = db.Catalogs.Where(d => d.ID != IgnoreID).ToList();
+
+            return result.AsQueryable()
+                .Select(d => new SelectListItem
+                {
+                    Value = d.ID.ToString(),
+                    Text = d.Title
+                });
+        }
+    }
+}
