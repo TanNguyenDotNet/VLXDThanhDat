b74ca669d2b651e602dfef3d32d9e4bb4c91aa54
anhphanhcm <anhphan.hcm@gmail.com>
2015-12-16 17:19:17 +0700

    add all objects

---
+++ MVCProject/Controllers/ProductNameController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCProject.Models;
+
+namespace MVCProject.Controllers
+{
+    public class ProductNameController : Controller
+    {
+        private aspnetEntities db = new aspnetEntities();
+
+        // GET: /ProductName/
+        public ActionResult Index()
+        {
+            return View(db.ProductNames.ToList());
+        }
+
+        // GET: /ProductName/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductName productname = db.ProductNames.Find(id);
+            if (productname == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productname);
+        }
+
+        // GET: /ProductName/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /ProductName/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="ID,ProductID,Name,LocationID")] ProductName productname)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ProductNames.Add(productname);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(productname);
+        }
+
+        // GET: /ProductName/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductName productname = db.ProductNames.Find(id);
+            if (productname == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productname);
+        }
+
+        // POST: /ProductName/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ID,ProductID,Name,LocationID")] ProductName productname)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(productname).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(productname);
+        }
+
+        // GET: /ProductName/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductName productname = db.ProductNames.Find(id);
+            if (productname == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productname);
+        }
+
+        // POST: /ProductName/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            ProductName productname = db.ProductNames.Find(id);
+            db.ProductNames.Remove(productname);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVCProject/Controllers/ProductPriceController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCProject.Models;
+
+namespace MVCProject.Controllers
+{
+    public class ProductPriceController : Controller
+    {
+        private aspnetEntities db = new aspnetEntities();
+
+        // GET: /ProductPrice/
+        public ActionResult Index()
+        {
+            return View(db.ProductPrices.ToList());
+        }
+
+        // GET: /ProductPrice/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductPrice productprice = db.ProductPrices.Find(id);
+            if (productprice == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productprice);
+        }
+
+        // GET: /ProductPrice/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /ProductPrice/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="ID,ProductID,Price,Created,Description,UserID,LocationID")] ProductPrice productprice)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ProductPrices.Add(productprice);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(productprice);
+        }
+
+        // GET: /ProductPrice/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductPrice productprice = db.ProductPrices.Find(id);
+            if (productprice == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productprice);
+        }
+
+        // POST: /ProductPrice/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ID,ProductID,Price,Created,Description,UserID,LocationID")] ProductPrice productprice)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(productprice).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(productprice);
+        }
+
+        // GET: /ProductPrice/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductPrice productprice = db.ProductPrices.Find(id);
+            if (productprice == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productprice);
+        }
+
+        // POST: /ProductPrice/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            ProductPrice productprice = db.ProductPrices.Find(id);
+            db.ProductPrices.Remove(productprice);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
