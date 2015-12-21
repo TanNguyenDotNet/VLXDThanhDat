23e644ab624d4b6683117443416add85c5928680
anhphanhcm <anhphan.hcm@gmail.com>
2015-12-21 09:20:11 +0700

    Add package

---
+++ MVCProject/Controllers/ProductCodeController.cs
@@ -0,0 +1,81 @@
+﻿using MVCProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+
+namespace MVCProject.Controllers
+{
+    public class ProductCodeController : Controller
+    {
+        private aspnetEntities db = new aspnetEntities();
+        //
+        // GET: /ProductCode/Create
+        public ActionResult Create()
+        {
+            if (!Common.Commons.CheckLogin(Request, Response, User.Identity.GetUserName()))
+                return null;
+
+            string id = Request.QueryString["code"];
+            List<Models.ProductCode> item = db.ProductCodes.Where(d => (bool)d.Active == true && d.CatCode == id).ToList();
+            if (item != null && item.Count > 0)
+                return View(item[0]);
+            else
+            {
+                return View(new Models.ProductCode { CatCode = id });
+            }
+        }
+
+        //
+        // POST: /ProductCode/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection, [Bind(Include = "Group1,Group2,CatID,ScrollNumber,ID,Active,CatCode")] ProductCode pc)
+        {
+            if (!Common.Commons.CheckLogin(Request, Response, User.Identity.GetUserName()))
+                return null;
+            try
+            {
+                if (!Request.IsAuthenticated)
+                    return null;
+
+                if (ModelState.IsValid)
+                {
+                    List<Models.ProductCode> item = db.ProductCodes.Where(d => d.Group1 == pc.Group1 && 
+                        d.Group2 == pc.Group2 && d.CatCode == pc.CatCode).ToList();
+
+                    foreach (Models.ProductCode c in db.ProductCodes)
+                        if (c.CatCode == pc.CatCode)
+                            c.Active = false;
+                    
+                    if (item == null || item.Count == 0)
+                    {
+                        pc.Active = true;
+                        db.ProductCodes.Add(pc);
+                    }
+                    else
+                    {
+                        var i = db.ProductCodes.Find(item[0].ID);
+                        i.Group1 = pc.Group1;
+                        i.Group2 = pc.Group2;
+                        i.CatID = pc.CatID;
+                        i.Active = true;
+                    }
+
+                    db.SaveChanges();
+                    if (pc.CatCode == "SP")
+                        return RedirectToAction("../Product/Index");
+                    else
+                        return RedirectToAction("../Promotion/Index");
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
